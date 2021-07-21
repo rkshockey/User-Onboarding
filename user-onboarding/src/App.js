abdc78cacf5470649ad80cd95403afe7d11f9f26
rkshockey <rkshockey@gmail.com>
2021-07-21 14:45:41 -0400

    Began axios request for user list

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from './components/Form'
+import axios from 'axios'
 
 function App() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => console.log(res))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
       <Form />
